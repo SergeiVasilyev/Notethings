51a99abd7c3a1aa8f4faa248cbf4c7b3a80fb752
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2023-12-06 18:01:36 +0200

    Settings: add appname, AUTH_USER_MODEL

--- notethings/settings.py
+++ notethings/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'note',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'note.CustomUser'
+
