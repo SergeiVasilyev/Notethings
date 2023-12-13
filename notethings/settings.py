3b33fe8d34649212c05c4aec275283fdcebf544c
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2023-12-13 16:35:34 +0200

    STATIC_URL

--- notethings/settings.py
+++ notethings/settings.py
@@ -121,7 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/noteapp/images')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- notethings/urls.py
+++ notethings/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from noteapp.views import main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
