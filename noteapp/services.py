f1d5953527e197ff71a0e100178d200531ca1769
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2023-12-17 12:19:46 +0200

    Del create_note

--- noteapp/services.py
+++ noteapp/services.py
@@ -1,6 +1,3 @@
 from noteapp.models import Note
 
 
-def create_note(name=None, text=None, creator=None, category=None, folder=None):
-    note = Note.create_note(name, text, creator, category, folder)
-    return note
