9dbc73d524405998afb17492fda387affe07375e
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-01-18 14:44:05 +0200

    fixed model. delete ckediotr field

--- noteapp/models.py
+++ noteapp/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.template.defaulttags import register
-from ckeditor.fields import RichTextField
-from ckeditor_uploader.fields import RichTextUploadingField 
 
 
 class CustomUser(AbstractUser, PermissionsMixin):
@@ -46,7 +44,7 @@ class Note(models.Model):
     )
 
     name = models.CharField(max_length=100, blank=True, null=True)
-    text = RichTextUploadingField(blank=True, null=True)
+    text = models.TextField(blank=True, null=True)
     creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     collaborators = models.ManyToManyField(CustomUser, related_name='collaborators')
     is_private = models.BooleanField(default=True)
